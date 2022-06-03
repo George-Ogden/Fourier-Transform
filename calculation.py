6448b1b362cf7e8cb684d2148162c348188e468e
George Ogden <George-Ogden@outlook.com>
2022-06-03 11:00:11 +0100

    truncated fft

--- calculation.py
+++ calculation.py
@@ -1,18 +1,16 @@
 from svgpathtools import *
 import numpy as np
-import scipy.fft
 
 import matplotlib.pyplot as plt
 paths, attributes = svg2paths('github.svg')
 
 points = np.array([shape.points(np.linspace(0,1,100)) for path in paths for shape in path]).reshape(-1)
 # points = np.array((points.real, points.imag))
-fft = scipy.fft.fft2((points.real, points.imag))
-print(fft.shape)
-points = scipy.fft.ifft2(fft[:,:1000])
+fft = np.fft.fft(points)
+fft[np.argsort(abs(fft))[:-50]] = 0
 
-# print(fft)
-print(points.shape)
+points = np.fft.ifft(fft)
 
-plt.scatter(*points)
+
+plt.scatter(points.real, points.imag)
 plt.show()
