6db6ec70e882a48c2aa2ca47eca1479c0d0a685d
George Ogden <George-Ogden@outlook.com>
2022-06-03 11:15:11 +0100

    created functions

--- calculation.py
+++ calculation.py
@@ -1,16 +1,9 @@
-from svgpathtools import *
+from svgpathtools import svg2paths
 import numpy as np
 
-import matplotlib.pyplot as plt
-paths, attributes = svg2paths('github.svg')
+def load(filename):
+    paths, _ = svg2paths(filename)
+    return np.array([shape.points(np.linspace(0,1,100)) for path in paths for shape in path]).reshape(-1)
 
-points = np.array([shape.points(np.linspace(0,1,100)) for path in paths for shape in path]).reshape(-1)
-# points = np.array((points.real, points.imag))
-fft = np.fft.fft(points)
-fft[np.argsort(abs(fft))[:-50]] = 0
-
-points = np.fft.ifft(fft)
-
-
-plt.scatter(points.real, points.imag)
-plt.show()
+def fft(points):
+    return np.fft.fft(points)
