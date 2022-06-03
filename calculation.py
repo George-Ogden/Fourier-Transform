5bd6863da140f59c3a20e7607b5acacb16860b3c
George Ogden <George-Ogden@outlook.com>
2022-06-03 14:48:12 +0100

    updated FFT

--- calculation.py
+++ calculation.py
@@ -3,7 +3,17 @@ import numpy as np
 
 def load(filename):
     paths, _ = svg2paths(filename)
-    return np.array([shape.points(np.linspace(0,1,100)) for path in paths for shape in path]).reshape(-1)
+    points = np.array([shape.points(np.linspace(0,1,100)) for path in paths for shape in path]).reshape(-1)
+    points -= points.mean()
+    points /= max(abs(points))
+    return points
 
-def fft(points):
-    return np.fft.fft(points)
+def fft(points, n):
+    coefficients = np.fft.fft(points,norm="forward")
+    frequencies = np.fft.fftfreq(len(points),1/len(points))
+    indices = np.argsort(-abs(coefficients))[:n]
+    frequencies = frequencies[indices]
+    coefficients = coefficients[indices]
+    phases = np.angle(coefficients)
+    amplitudes = abs(coefficients)
+    return amplitudes, frequencies, phases
