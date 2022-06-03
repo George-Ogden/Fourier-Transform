11159db674093b2d82fd9708fe7cb250f40416ae
George Ogden <George-Ogden@outlook.com>
2022-06-03 10:35:38 +0100

    fft working

--- calculation.py
+++ calculation.py
@@ -1,10 +1,18 @@
 from svgpathtools import *
 import numpy as np
+import scipy.fft
+
 import matplotlib.pyplot as plt
 paths, attributes = svg2paths('github.svg')
 
-points = np.array([shape.points(np.linspace(0,1,100)) for path in paths for shape in path])
-points = np.array((points.real, points.imag))
+points = np.array([shape.points(np.linspace(0,1,100)) for path in paths for shape in path]).reshape(-1)
+# points = np.array((points.real, points.imag))
+fft = scipy.fft.fft2((points.real, points.imag))
+print(fft.shape)
+points = scipy.fft.ifft2(fft[:,:1000])
+
+# print(fft)
+print(points.shape)
 
 plt.scatter(*points)
 plt.show()
